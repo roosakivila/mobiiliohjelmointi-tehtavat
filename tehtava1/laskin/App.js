b358d5e372bad037835559a71e88d577ea38ef38
Roosa Kivilä <roosakivila@Roosa-MacBook-Air.local>
2025-08-26 11:40:16 +0300

    Tehtävä laskin historialla valmis, tehty samaan tiedostoon kun laskin

--- tehtava1/laskin/App.js
+++ tehtava1/laskin/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 import { useState } from 'react';
 
 
@@ -8,19 +8,28 @@ export default function App() {
   const [number1, setNumber1] = useState('');
   const [number2, setNumber2] = useState('');
   const [result, setResult] = useState('');
+  const [history, setHistory] = useState([]);
 
   const handleAdd = () => {
     const sum = parseFloat(number1) + parseFloat(number2);
     setResult(sum.toString());
+    const historyString = `${number1} + ${number2} = ${sum}`;
+    setHistory([...history, historyString]);
+    setNumber1('');
+    setNumber2('');
   }
 
   const handleSubtract = () => {
     const sub = parseFloat(number1) - parseFloat(number2);
     setResult(sub.toString());
+    const historyString = `${number1} - ${number2} = ${sub}`;
+    setHistory([...history, historyString]);
+    setNumber1('');
+    setNumber2('');
   }
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text style={styles.title}>Result: {result}</Text>
       <TextInput
         style={styles.input}
         onChangeText={number1 => setNumber1(number1)}
@@ -35,6 +44,12 @@ export default function App() {
         <Button title="+" onPress={handleAdd} />
         <Button title="-" onPress={handleSubtract} />
       </View>
+      <Text style={styles.title}>History</Text>
+      <FlatList
+        data={history}
+        renderItem={({ item }) => <Text style={styles.historyText}>{item}</Text>}
+        style={styles.historyList}>
+      </FlatList>
       <StatusBar style="auto" />
     </View>
   );
@@ -43,6 +58,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
@@ -51,8 +67,21 @@ const styles = StyleSheet.create({
     height: 40,
     width: 80,
     margin: 12,
-    borderBlockColor: 'black',
+    borderColor: 'black',
     borderWidth: 1,
     padding: 10,
   },
+  historyList: {
+    height: 50,
+    width: '80%',
+    marginTop: 10,
+  },
+  historyText: {
+    textAlign: 'center'
+  },
+title: {
+  fontWeight: 'bold',
+  fontSize: 15,
+  paddingTop: 50,
+}
 });
